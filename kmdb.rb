df46d302870d8a1f7382045e5b4fb97d8a86d7f0
Jeriel Acosta <jeriel.acosta@kellogg.northwestern.edu>
2023-05-02 18:34:05 +0000

    movies query

--- kmdb.rb
+++ kmdb.rb
@@ -266,6 +266,17 @@ puts ""
 
 # Query the movies data and loop through the results to display the movies output.
 # TODO!
+movies = Movie.all
+
+for movie in movies
+    title = movie["title"]
+    year_released = movie["year_released"]
+    rated = movie ["rated"]
+    studio = Studio.find_by({"id" => movie["studio_id"]})
+    studio_name = studio["name"]
+    
+    puts "#{title} #{year_released} #{rated} #{studio_name}"
+end
 
 # Prints a header for the cast output
 puts ""
